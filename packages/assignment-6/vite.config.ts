7b4f00cfc40d797d1a9503bb6f0fdc5cf62f9f0a
tsihnavy99 <tsihnavy99@naver.com>
2024-07-22 23:44:12 +0900

    job3 vite.config 수정
    job3 vite.config 수정

--- packages/assignment-6/vite.config.ts
+++ packages/assignment-6/vite.config.ts
@@ -1,16 +1,22 @@
-import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    test: {
+      coverage: {
+        provider: "istanbul",
+        reporter: ["text", "json", "html"],
+      },
+    },
   }),
   defineTestConfig({
     test: {
       globals: true,
-      environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.ts",
     },
   })
-)
+);
