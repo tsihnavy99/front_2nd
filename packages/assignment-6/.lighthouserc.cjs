a42ae7fda5b6acbee0e332e3dd1ac859f554814f
tsihnavy99 <tsihnavy99@naver.com>
2024-07-26 01:00:04 +0900

    job4 test
    job4 test

--- packages/assignment-6/.lighthouserc.cjs
+++ packages/assignment-6/.lighthouserc.cjs
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       startServerCommand: "pnpm -F assignment-6 start",
-      url: ["http://localhost:3000"],
+      url: ["http://localhost:5173"],
     },
     upload: {
       target: "filesystem",
