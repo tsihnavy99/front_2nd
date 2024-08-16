d0e584540ce3874262cc2f32a0e33152b6f83e57
junilhwang <junil.h@kakao.com>
2024-08-17 06:19:08 +0900

    feat: 10주차 과제 추가

--- packages/assignment-10/src/basic/SearchDialog.tsx
+++ packages/assignment-10/src/basic/SearchDialog.tsx
@@ -82,15 +82,20 @@ const TIME_SLOTS = [
 
 const PAGE_SIZE = 20;
 
-const fetchAllLectures = () => Promise.all([
-  axios.get<Lecture[]>('/schedules-majors.json'),
-  axios.get<Lecture[]>('/schedules-liberal-arts.json'),
-  axios.get<Lecture[]>('/schedules-majors.json'),
-  axios.get<Lecture[]>('/schedules-liberal-arts.json'),
-  axios.get<Lecture[]>('/schedules-majors.json'),
-  axios.get<Lecture[]>('/schedules-liberal-arts.json'),
+const fetchMajors = () => axios.get<Lecture[]>('/schedules-majors.json');
+const fetchLiberalArts = () => axios.get<Lecture[]>('/schedules-majors.json');
+
+// TODO: 이 코드를 개선해서 API 호출을 최소화 해보세요 + Promise.all이 현재 잘못 사용되고 있습니다. 같이 개선해주세요.
+const fetchAllLectures = async () => await Promise.all([
+  (console.log('API Call 1', performance.now()), await fetchMajors()),
+  (console.log('API Call 2', performance.now()), await fetchLiberalArts()),
+  (console.log('API Call 3', performance.now()), await fetchMajors()),
+  (console.log('API Call 4', performance.now()), await fetchLiberalArts()),
+  (console.log('API Call 5', performance.now()), await fetchMajors()),
+  (console.log('API Call 6', performance.now()), await fetchLiberalArts()),
 ]);
 
+// TODO: 이 컴포넌트에서 불필요한 연산이 발생하지 않도록 다양한 방식으로 시도해주세요.
 const SearchDialog = ({ searchInfo, onClose }: Props) => {
   const { setSchedulesMap } = useScheduleContext();
 
@@ -162,7 +167,12 @@ const SearchDialog = ({ searchInfo, onClose }: Props) => {
   };
 
   useEffect(() => {
+    const start = performance.now();
+    console.log('API 호출 시작: ', start)
     fetchAllLectures().then(results => {
+      const end = performance.now();
+      console.log('모든 API 호출 완료 ', end)
+      console.log('API 호출에 걸린 시간(ms): ', end - start)
       setLectures(results.flatMap(result => result.data));
     })
   }, []);
