8c1c379d820a6044081e4434b0c9a9cb428aa503
junilhwang <junil.h@kakao.com>
2024-08-17 12:48:46 +0900

    feat: 10주차 과제 일부 수정

--- packages/assignment-10/src/basic/SearchDialog.tsx
+++ packages/assignment-10/src/basic/SearchDialog.tsx
@@ -83,7 +83,7 @@ const TIME_SLOTS = [
 const PAGE_SIZE = 100;
 
 const fetchMajors = () => axios.get<Lecture[]>('/schedules-majors.json');
-const fetchLiberalArts = () => axios.get<Lecture[]>('/schedules-majors.json');
+const fetchLiberalArts = () => axios.get<Lecture[]>('/schedules-liberal-arts.json');
 
 // TODO: 이 코드를 개선해서 API 호출을 최소화 해보세요 + Promise.all이 현재 잘못 사용되고 있습니다. 같이 개선해주세요.
 const fetchAllLectures = async () => await Promise.all([
